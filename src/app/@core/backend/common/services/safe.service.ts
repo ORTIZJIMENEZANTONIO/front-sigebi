2e000c6d2cdac2c9725ea4cc8cc4b55e888ecdc4
ORTIZJIMENEZANTONIO <joseantonioescom@gmail.com>
2022-08-05 18:22:47 -0500

    merged

--- src/app/@core/backend/common/services/safe.service.ts
+++ src/app/@core/backend/common/services/safe.service.ts
@@ -4,11 +4,8 @@ import { Observable, of } from 'rxjs';
 import { DataSource } from 'ng2-smart-table/lib/lib/data-source/data-source';
 import { CatalogApi } from '../api/catalog-api'; 
 
-<<<<<<< HEAD
 import { SafeInterface } from '../../../interfaces/auction/safe.model';
-=======
 import { SettlementInterface } from '../../../interfaces/auction/settlement.model';
->>>>>>> d4fe1c789adbdcfe2c0f75e2b96ee870fd90b280
 
 @Injectable()
 export class SafeService {
@@ -26,27 +23,16 @@ export class SafeService {
     return data;
   }
 
-<<<<<<< HEAD
   register( data: SafeInterface ): Observable<SafeInterface>{
     return this.api.register( data, this.url );
   }
 
-  update(id:number, data: SafeInterface): Observable<SafeInterface>{
-=======
-  register( data: SettlementInterface ): Observable<SettlementInterface>{
-    return this.api.register( data, this.url );
-  }
-
   update(id:number, data: SettlementInterface): Observable<SettlementInterface>{
->>>>>>> d4fe1c789adbdcfe2c0f75e2b96ee870fd90b280
     return this.api.update( id, data, this.url );
   }
 
   delete(id:number){
     return this.api.delete( id, this.url );
   }
-<<<<<<< HEAD
-  
-=======
->>>>>>> d4fe1c789adbdcfe2c0f75e2b96ee870fd90b280
+
 }
