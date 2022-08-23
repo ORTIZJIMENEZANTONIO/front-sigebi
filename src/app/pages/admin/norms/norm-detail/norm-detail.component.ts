b24f5ce827bc71e44487943bf2c654077b614f68
AbnerMendoza <abnerjmendoza@gmail.com>
2022-08-23 17:44:14 -0400

    corrections of forms validations, STATUS_PROCESO FRACTIONS GENERICS OFFICIAL_LEGENDS LOCATION REASONS_NO_DELIVERY MUNICIPALITY RULES PARAGRAPHS

--- src/app/pages/admin/norms/norm-detail/norm-detail.component.ts
+++ src/app/pages/admin/norms/norm-detail/norm-detail.component.ts
@@ -1,76 +1,109 @@
 import { ChangeDetectorRef, Component, Inject, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { NbWindowRef, NB_WINDOW_CONTEXT, NbWindowService } from '@nebular/theme';
+import { NbWindowRef, NB_WINDOW_CONTEXT, NbWindowService, NbToastrService } from '@nebular/theme';
 import { DomSanitizer } from '@angular/platform-browser';
 import { BaseApp } from '../../../../@core/shared/base-app';
 import { STRING_PATTERN } from '../../../../@components/constants';
 import { NormService } from '../../../../@core/backend/common/services/norm.service';
+import { SweetAlertConstants } from '../../../../@core/interfaces/auction/sweetalert-model';
+import { NormModel } from '../../../../@core/interfaces/auction/norm.model';
+import { BasePage } from '../../../../@core/shared/base-page';
 
 @Component({
   selector: 'ngx-norm-detail',
   templateUrl: './norm-detail.component.html',
   styleUrls: ['./norm-detail.component.scss']
 })
-export class NormDetailComponent extends BaseApp {
-  norm: any = {};
-
-  constructor(private fb: FormBuilder, protected cd: ChangeDetectorRef, protected router: Router, private service: NormService,
-    public windowRef: NbWindowRef, @Inject(NB_WINDOW_CONTEXT) context, private dom: DomSanitizer,  private windowService: NbWindowService) { 
-      super();
-      if (null != context.norm){
-        this.norm = context.norm;
-      }
+export class NormDetailComponent extends BasePage {
+  public formNorm: FormGroup;
+  private norm: NormModel;
+  public actionBtn: string = "Guardar";
+  constructor(
+    private fb: FormBuilder,
+    protected cd: ChangeDetectorRef,
+    protected router: Router,
+    private service: NormService,
+    public windowRef: NbWindowRef,
+    public toastrService: NbToastrService,
+    @Inject(NB_WINDOW_CONTEXT) context) {
+    super(toastrService);
+    if (null != context.norm) {
+      this.norm = context.norm;
     }
-    actionBtn : string = "Guardar";
+  }
 
-    formNorm = this.fb.group({
+  ngOnInit(): void {
+    this.prepareForm();
+  }
+  private prepareForm() {
+    this.formNorm = this.fb.group({
       norm: [null, Validators.compose([Validators.pattern("[a-zA-Z]((\.|_|-)?[a-zA-ZáéíóúÁÉÍÓÚ\u0020]+){0,255}"), Validators.required])],
       article: [null, Validators.compose([Validators.required])],
-      type: [null, Validators.compose([Validators.pattern("[0-9]{0,255}"),Validators.required])],
+      type: [null, Validators.compose([Validators.pattern("[0-9]{0,255}"), Validators.required])],
       characteristics: [null, Validators.compose([Validators.required])],
       merchandise: [null, Validators.compose([Validators.required])],
       fundament: [null, Validators.compose([Validators.required])],
       objective: [null, Validators.compose([Validators.required])],
-      destination: [null, Validators.compose([Validators.pattern("[0-9]{0,255}"),Validators.required])],
-      condition: [null, Validators.compose([Validators.pattern("[0-9]{0,255}"),Validators.required])],
-      version: [null, Validators.compose([Validators.pattern("^([0-9]+)+([.][0-9]+)+([.][0-9]+)?$"),Validators.required])],
-      status: [null, Validators.compose([ Validators.maxLength(20), Validators.required])],
+      destination: [null, Validators.compose([Validators.pattern("[0-9]{0,255}"), Validators.required])],
+      condition: [null, Validators.compose([Validators.pattern("[0-9]{0,255}"), Validators.required])],
+      version: [null, Validators.compose([Validators.pattern(""), Validators.required])],
+      status: [null, Validators.compose([Validators.maxLength(20), Validators.required])],
     });
-  
-    get validateNorm(){
-      return this.formNorm.controls;
-    }
-    
-  ngOnInit(): void {
-    
-    if(this.norm.id){
+    if (this.norm) {
       this.actionBtn = "Actualizar";
       this.formNorm.patchValue(this.norm)
+
     }
-    
   }
 
-  register(): void {
-    const data = this.formNorm.value;
-    data.modificationDate = Date();
-    if( this.actionBtn == "Guardar"){
+  public get normF() { return this.formNorm.get('norm'); }
+  public get article() { return this.formNorm.get('noRegistration'); }
+  public get dict_ofi() { return this.formNorm.get('dict_ofi'); }
+  public get type() { return this.formNorm.get('type'); }
+  public get characteristics() { return this.formNorm.get('characteristics'); }
+  public get merchandise() { return this.formNorm.get('merchandise'); }
+  public get fundament() { return this.formNorm.get('fundament'); }
+  public get objective() { return this.formNorm.get('objective'); }
+  public get destination() { return this.formNorm.get('destination'); }
+  public get condition() { return this.formNorm.get('condition'); }
+  public get version() { return this.formNorm.get('version'); }
+  public get estatus() { return this.formNorm.get('status'); }
 
-      data.userCreation ="Rafael";
-      data.userModification = "Antonio";
-      data.creationDate = Date();
-
-      this.service.register(data).subscribe(data =>{
+  public register(): void {
+    const data = this.formNorm.getRawValue();
+    this.actionBtn == "Guardar" ? this.createRegister(data) : this.updateRegister(data);
+  }
+  private createRegister(data): void {
+    this.service.register(data).subscribe(
+      () => {
+        this.onLoadFailed('success', 'Despacho', 'Registrado Correctamente');
+      }, err => {
+        let error = '';
+        if (err.status === 0) {
+          error = SweetAlertConstants.noConexion;
+        } else {
+          error = err.message;
+        }
+        this.onLoadFailed('danger', 'Error', error);
+      }, () => {
         this.windowRef.close();
-      },err =>{
-        console.log(err);
-      })
-    }else{
-      this.service.update(this.norm.id,data).subscribe(data =>{
+      });
+  }
+  private updateRegister(data): void {
+    this.service.update(this.norm.id, data).subscribe(
+      () => {
+        this.onLoadFailed('success', 'Despacho', 'Actualizado Correctamente');
+      }, err => {
+        let error = '';
+        if (err.status === 0) {
+          error = SweetAlertConstants.noConexion;
+        } else {
+          error = err.message;
+        }
+        this.onLoadFailed('danger', 'Error', error);
+      }, () => {
         this.windowRef.close();
-      },err =>{
-        console.log(err);
-      })
-    }
+      });
   }
 }
--- src/app/pages/admin/status-process/status-process-detail/status-process-detail.component.ts
+++ src/app/pages/admin/status-process/status-process-detail/status-process-detail.component.ts
@@ -1,69 +1,92 @@
 import { ChangeDetectorRef, Component, Inject, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { NbWindowRef, NB_WINDOW_CONTEXT, NbWindowService } from '@nebular/theme';
+import { NbWindowRef, NB_WINDOW_CONTEXT, NbWindowService, NbToastrService } from '@nebular/theme';
 import { DomSanitizer } from '@angular/platform-browser';
 import { BaseApp } from '../../../../@core/shared/base-app';
 import { STRING_PATTERN } from '../../../../@components/constants';
 import { StatusProcessService } from '../../../../@core/backend/common/services/statusProcess.service';
+import { BasePage } from '../../../../@core/shared/base-page';
+import { StatusProcessModel } from '../../../../@core/interfaces/auction/statusProcess.model';
+import { SweetAlertConstants } from '../../../../@core/interfaces/auction/sweetalert-model';
 
 @Component({
   selector: 'ngx-status-process-detail',
   templateUrl: './status-process-detail.component.html',
   styleUrls: ['./status-process-detail.component.scss']
 })
-export class StatusProcessDetailComponent extends BaseApp {
-  statusProcess: any = {};
-
-  constructor(private fb: FormBuilder, protected cd: ChangeDetectorRef, protected router: Router, private service: StatusProcessService,
-    public windowRef: NbWindowRef, @Inject(NB_WINDOW_CONTEXT) context, private dom: DomSanitizer,  private windowService: NbWindowService) { 
-      super();
-      if (null != context.statusProcess){
-        this.statusProcess = context.statusProcess;
-      }
+export class StatusProcessDetailComponent extends BasePage {
+  public formStatusProcess: FormGroup;
+  private data: StatusProcessModel;
+  public actionBtn: string = "Guardar";
+  constructor(
+    private fb: FormBuilder,
+    protected cd: ChangeDetectorRef,
+    protected router: Router,
+    private service: StatusProcessService,
+    public windowRef: NbWindowRef,
+    public toastrService: NbToastrService,
+    @Inject(NB_WINDOW_CONTEXT) context) {
+    super(toastrService);
+    if (null != context.data) {
+      this.data = context.data;
     }
-    actionBtn : string = "Guardar";
+  }
 
-    formStatusProcess = this.fb.group({
+  ngOnInit(): void {
+    this.prepareForm();
+  }
+  private prepareForm() {
+    this.formStatusProcess = this.fb.group({
       status: [null, Validators.compose([Validators.pattern("[a-zA-Z]((\.|_|-)?[a-zA-ZáéíóúÁÉÍÓÚ\u0020]+){0,255}"), Validators.required])],
       process: [null, Validators.compose([Validators.pattern("[a-zA-Z]((\.|_|-)?[a-zA-ZáéíóúÁÉÍÓÚ\u0020]+){0,255}"), Validators.required])],
       description: [null, Validators.compose([Validators.required])],
     });
-  
-    get validateStatusProcess(){
-      return this.formStatusProcess.controls;
-    }
-    
-  ngOnInit(): void {
-    
-    if(this.statusProcess.status){
+    if (this.data) {
       this.actionBtn = "Actualizar";
-      this.formStatusProcess.patchValue(this.statusProcess);
-      this.formStatusProcess.controls['status'].disable();
+      this.formStatusProcess.patchValue(this.data);
+      this.estatus.disable();
     }
-    
   }
 
-  register(): void {
-    const data = this.formStatusProcess.value;
-    data.modificationDate = Date();
-    if( this.actionBtn == "Guardar"){
+  public get estatus() { return this.formStatusProcess.get('status'); }
+  public get process() { return this.formStatusProcess.get('process'); }
+  public get description() { return this.formStatusProcess.get('description'); }
 
-      data.userCreation ="Rafael";
-      data.userModification = "Antonio";
-      data.creationDate = Date();
-
-      this.service.register(data).subscribe(data =>{
+  public register(): void {
+    const data = this.formStatusProcess.getRawValue();
+    this.actionBtn == "Guardar" ? this.createRegister(data) : this.updateRegister(data);
+  }
+  private createRegister(data): void {
+    this.service.register(data).subscribe(
+      () => {
+        this.onLoadFailed('success', 'Despacho', 'Registrado Correctamente');
+      }, err => {
+        let error = '';
+        if (err.status === 0) {
+          error = SweetAlertConstants.noConexion;
+        } else {
+          error = err.message;
+        }
+        this.onLoadFailed('danger', 'Error', error);
+      }, () => {
         this.windowRef.close();
-      },err =>{
-        console.log(err);
-      })
-    }else{
-      this.service.update(this.statusProcess.status,data).subscribe(data =>{
+      });
+  }
+  private updateRegister(data): void {
+    this.service.update(this.data.status, data).subscribe(
+      () => {
+        this.onLoadFailed('success', 'Despacho', 'Actualizado Correctamente');
+      }, err => {
+        let error = '';
+        if (err.status === 0) {
+          error = SweetAlertConstants.noConexion;
+        } else {
+          error = err.message;
+        }
+        this.onLoadFailed('danger', 'Error', error);
+      }, () => {
         this.windowRef.close();
-      },err =>{
-        console.log(err);
-      })
-    }
+      });
   }
 }
