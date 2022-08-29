367e388f4cbc177ac7351cd8ad01932188155d17
Paola Chicaiza <pao.paty.chicaiza@gmail.com>
2022-08-29 09:20:06 -0500

    back cat_abogados y cat_almacenes

--- src/app/@core/backend/common/services/warehouse.service.ts
+++ src/app/@core/backend/common/services/warehouse.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { DataSource } from 'ng2-smart-table/lib/lib/data-source/data-source';
 
+import { DataSource } from 'ng2-smart-table/lib/lib/data-source/data-source';
 import { CatalogApi } from '../api/catalog-api'; 
 import { WarehouseInterface } from '../../../interfaces/auction/warehouse.model';
 
@@ -32,4 +32,8 @@ export class WarehouseService {
   delete(id:number){
     return this.api.delete( id, this.url );
   }
+
+  search(text:string){
+    return this.api.search(text,this.url);
+}
 }
