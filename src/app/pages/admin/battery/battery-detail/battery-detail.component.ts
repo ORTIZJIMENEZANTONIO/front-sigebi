12174c9d7baf603f215621329c0767ba5bb35354
Asynfur <joshualtdidk@gmail.com>
2022-08-30 13:03:45 -0400

    Correcciones

--- src/app/pages/admin/battery/battery-detail/battery-detail.component.ts
+++ src/app/pages/admin/battery/battery-detail/battery-detail.component.ts
@@ -2,12 +2,13 @@ import { ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NbToastrService, NbWindowRef, NB_WINDOW_CONTEXT } from '@nebular/theme';
-import { BehaviorSubject } from 'rxjs';
-import { STRING_PATTERN } from '../../../../@components/constants';
-import { BatteryService } from '../../../../@core/backend/common/services/battery.service';
-import { BatteryInterface } from '../../../../@core/interfaces/auction/battery.model';
 import { SweetAlertConstants } from '../../../../@core/interfaces/auction/sweetalert-model';
 import { BasePage } from '../../../../@core/shared/base-page';
+import { BehaviorSubject } from 'rxjs';
+import { NUMBERS_PATTERN, STRING_PATTERN } from '../../../../@components/constants';
+import { BatteryService } from '../../../../@core/backend/common/services/battery.service';
+import { BatteryInterface } from '../../../../@core/interfaces/auction/battery.model';
+import { SaveValuesService } from '../../../../@core/backend/common/services/save-values.service';
 
 @Component({
   selector: 'ngx-battery-detail',
@@ -27,6 +28,7 @@ export class BatteryDetailComponent extends BasePage implements OnInit {
     private service: BatteryService,
     public windowRef: NbWindowRef,
     public toastrService: NbToastrService,
+    private saveService: SaveValuesService,
     @Inject(NB_WINDOW_CONTEXT) context
   ) {
     super(toastrService);
@@ -40,9 +42,9 @@ export class BatteryDetailComponent extends BasePage implements OnInit {
   private prepareForm(): void {
 
     this.form = this.fb.group({
-    idBattery:[null],
+    idBattery:[null, Validators.compose([Validators.pattern(NUMBERS_PATTERN), Validators.required])],
     storeDesc: [ null, Validators.compose([ Validators.pattern(STRING_PATTERN)])],
-    description: [null, Validators.compose([Validators.pattern(""), Validators.required, Validators.maxLength(30)])],
+    description: [null, Validators.compose([Validators.pattern(STRING_PATTERN), Validators.required, Validators.maxLength(30)])],
     registerNumber: [null, Validators.compose([Validators.pattern(""), Validators.required, Validators.maxLength(20)])],
     status: [null, Validators.compose([Validators.pattern(""), Validators.required, Validators.maxLength(1)])],
     storeCode: [null, Validators.compose([Validators.pattern(""), Validators.required, Validators.maxLength(5)])]
@@ -56,16 +58,19 @@ export class BatteryDetailComponent extends BasePage implements OnInit {
       } 
       this.actionBtn = "Actualizar";
       this.form.patchValue(dataUpdt);
-      this.form.controls['storeCode'].disable();
+      this.form.controls['storeDesc'].disable();
+      this.form.controls['idBattery'].disable()
     }
-    this.form.controls['storeCode'].valueChanges.subscribe((value: string) => {
+    this.form.controls['storeDesc'].valueChanges.subscribe((value: string) => {
       if (value) {
-        this.service.search(value).subscribe(data => {
+        this.saveService.search(value).subscribe(data => {
           this.filteredOptions$.next(data);
         })
       }
     })
   }
+
+  public get idBattery() { return this.form.get('idBattery'); }
   public get storeDesc() { return this.form.get('storeDesc'); }
   public get storeCode() { return this.form.get('storeCode'); }
   public get description() { return this.form.get('description'); }
@@ -74,8 +79,9 @@ export class BatteryDetailComponent extends BasePage implements OnInit {
 
 
   public onSelectionChange(event){
-    if(event.idBattery){
-      this.form.controls['storeCode'].setValue(event.id);
+   
+    if(event.cve){
+      this.form.controls['storeCode'].setValue(event.cve);
       this.form.controls['storeDesc'].setValue(event.description);
     }
         
@@ -104,7 +110,7 @@ export class BatteryDetailComponent extends BasePage implements OnInit {
   private updateRegister(data): void {
     delete data.idBattery;
     delete data.storeCode;
-    this.service.update(this.data.idBattery, data).subscribe(
+    this.service.update( {idBattery:this.data.idBattery, storeCode: this.data.storeCode.cve}, data).subscribe(
       data => {
         this.onLoadFailed('success', 'Bateria', 'Actualizado Correctamente');
       }, err => {
